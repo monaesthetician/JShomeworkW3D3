2f619fd90fe31d6dbd3373a5b19747d4b6cb2e23
Mona <emonearth915@gmail.com>
2021-04-14 17:07:35 -0400

    W3D3 JS

---
+++ index.js
@@ -0,0 +1,36 @@
+var a = 3;
+var b = 2;
+var c = 34;
+var d = 8;
+var e = 5;
+var f = 9;
+var g = 5.99;
+var h = 9.95;
+var i = 5.25;
+var j = 12.95;
+
+// two times addition
+console.log(a+j);
+console.log(h+g);
+
+// two times substraction
+console.log(b-j);
+console.log(f-g);
+
+// two times multiplication
+console.log(e*b);
+console.log(f*d);
+
+// two times division
+console.log(c/b);
+console.log(f/a);
+
+// two times, increments
+console.log(j++);
+console.log(c++);
+
+
+
+//two times, decrements
+console.log(a--);
+console.log(d--);
